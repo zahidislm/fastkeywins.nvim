54b64ec30ff87ab019795b70b4814848ab79bc22
Zahid Islam <dev@zahidislm.com>
2023-12-01 03:14:56 -0500

    fix(config): remove dead code

--- lua/fastkeywins/config/init.lua
+++ lua/fastkeywins/config/init.lua
@@ -4,7 +4,7 @@ M.options = require("fastkeywins.config.defaults")
 
 function M.setup(options)
   options = options or {}
-  M.options = vim.tbl_deep_extend("force", {}, M.defaults(), options)
+  M.options = vim.tbl_deep_extend("force", {}, M.options, options)
 end
 
 return M
